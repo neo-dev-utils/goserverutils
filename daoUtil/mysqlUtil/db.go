bd044e41d78e475301c4c0e206eac6978a5b2381
lilan <lilan880523@gmail.com>
2025-04-01 01:59:32 +0800

    12

--- daoUtil/mysqlUtil/db.go
+++ daoUtil/mysqlUtil/db.go
@@ -3,14 +3,10 @@ package mysqlUtil
 import (
 	"context"
 	"fmt"
-	"io"
-	"log"
-	"os"
 	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 type Option struct {
@@ -36,33 +32,16 @@ func ConnectDB(ctx context.Context, o Option) (db *gorm.DB, err error) {
 		o.Timezone,
 	)
 
-	instance, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.New(
-			log.New(io.MultiWriter(writer, os.Stdout), "\r\n", log.LstdFlags),
-			logger.Config{
-				SlowThreshold:             time.Second,
-				Colorful:                  false,
-				IgnoreRecordNotFoundError: false,
-				LogLevel:                  logger.Warn,
-			},
-		),
-	})
+	instance, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Connect db error:\n" + err.Error())
 	}
+
 	pool, _ := instance.DB()
 	pool.SetMaxIdleConns(2)
 	pool.SetMaxOpenConns(4)
 	pool.SetConnMaxLifetime(3 * time.Hour)
 	db = instance
 
-	// db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	// panic(fmt.Errorf("connect db fail: %w", err))
-	// 	return nil, err
-	// }
-
-	// return db, nil
-
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -6,12 +6,14 @@ replace gocommutils => ../gocommutils
 
 require (
 	gocommutils v0.0.0-00010101000000-000000000000
+	gorm.io/driver/mysql v1.5.7
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
 )
 
 require (
 	github.com/dromara/carbon/v2 v2.5.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
